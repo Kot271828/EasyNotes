4a109b4c2533a388a87fe6e0426ab2667b824de3
Kot271828 <nbtgp528g@gmail.com>
2021-08-10 01:05:26 +0900

    Enable to rendering markdown

--- easy_notes/__init__.py
+++ easy_notes/__init__.py
@@ -2,12 +2,22 @@ from flask import Flask, render_template
 
 from . import notes
 
+
 def create_app():
     app = Flask(__name__)
 
-    @app.route('/')
+    @app.route("/")
     def index():
-        return render_template('index.html', all_notes={"notes": notes.all_notes_generator()})
-        #return render_template('index.html', all_notes={"notes": "d"})
+        return render_template(
+            "index.html", all_notes={"notes": notes.all_notes_generator()}
+        )
+
+    @app.route("/notes/<note_name>")
+    def notes_page(note_name):
+        note = {
+            "title": notes.get_note_title(note_name),
+            "body": notes.get_note_body(note_name),
+        }
+        return render_template("note.html", note=note)
 
     return app
--- easy_notes/notes.py
+++ easy_notes/notes.py
@@ -1,7 +1,18 @@
-
-
 from . import path
+import markdown
+
 
 def all_notes_generator():
     return path.NOTES.glob("*.md")
 
+
+def get_note_title(note_name):
+    return note_name[: len(note_name) - len(".md")]
+
+
+def get_note_body(note_name):
+    with open(path.NOTES / note_name, mode="r") as f:
+        text = f.read()
+    md = markdown.Markdown()
+    body_html = md.convert(text)
+    return body_html
