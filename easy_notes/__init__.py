24365bcd1bf62879800903504c482cdc0ab12fca
Kot271828 <nbtgp528g@gmail.com>
2021-08-10 00:34:33 +0900

    Change routing of /

--- easy_notes/__init__.py
+++ easy_notes/__init__.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
+
+from . import notes
 
 def create_app():
     app = Flask(__name__)
 
     @app.route('/')
-    def hello():
-        return 'hello'
+    def index():
+        return render_template('index.html', all_notes={"notes": notes.all_notes_generator()})
+        #return render_template('index.html', all_notes={"notes": "d"})
 
     return app
